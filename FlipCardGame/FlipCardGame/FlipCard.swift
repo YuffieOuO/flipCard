6a8756fbfad779d927f71d3945e79ed51d29d9db
Yuffie <69662158+YuffieOuO@users.noreply.github.com>
2020-11-05 15:49:33 +0800

    Add files via upload

---
+++ FlipCardGame/FlipCardGame/FlipCard.swift
@@ -0,0 +1,27 @@
+//
+//  FlipCard.swift
+//  FlipCardGame
+//
+//  Created by zencher on 2020/10/25.
+//
+
+import UIKit
+
+class FlipCard: UIImageView {
+    var isFront:Bool = false
+    var cardImageName:String = ""
+    var vc:ViewController?
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        isFront.toggle()
+        
+        if isFront {
+            image = UIImage(named: cardImageName)
+        } else {
+            image = UIImage(named: "background")
+        }
+                
+        vc?.fliping(card: self)
+    }
+
+}
---
+++ FlipCardGame/FlipCardGame/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  FlipCardGame
+//
+//  Created by zencher on 2020/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var card0: FlipCard!
+    @IBOutlet weak var card1: FlipCard!
+    @IBOutlet weak var card2: FlipCard!
+    @IBOutlet weak var card3: FlipCard!
+    @IBOutlet weak var card4: FlipCard!
+    @IBOutlet weak var card5: FlipCard!
+    var cardName: [String] = ["1", "1", "2", "2", "3", "3"]
+    var numberOfFlipCard: Int = 0
+    var timer = Timer()
+    var name:String?
+    var firstFlipCard:FlipCard!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cardName.shuffle()
+        card0.cardImageName = cardName[0]
+        card0.vc = self
+        card1.cardImageName = cardName[1]
+        card1.vc = self
+        card2.cardImageName = cardName[2]
+        card2.vc = self
+        card3.cardImageName = cardName[3]
+        card3.vc = self
+        card4.cardImageName = cardName[4]
+        card4.vc = self
+        card5.cardImageName = cardName[5]
+        card5.vc = self
+    }
+
+    func fliping(card:FlipCard) {
+        numberOfFlipCard += 1
+        if numberOfFlipCard == 1 {
+            firstFlipCard = card
+        } else if numberOfFlipCard % 2 == 0 {
+            if card.cardImageName == name {
+                print("Y")
+            } else {
+                Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { timer in
+                    card.image = UIImage(named: "background")
+                    self.firstFlipCard.image =  UIImage(named: "background")
+                    timer.invalidate()
+                    self.numberOfFlipCard = 0
+                }
+            }
+        }
+        name = card.cardImageName
+        print("press", card.cardImageName)
+    }
+    
+}
+
